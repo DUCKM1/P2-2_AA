7047fae0120c4d71a8d82718b676361dace47404
DUCKM1 <aandersons007@gmail.com>
2025-06-04 11:30:36 +0300

    .23

--- scripts.js
+++ scripts.js
@@ -2,12 +2,16 @@
 
 let pages = {
     'Web Akinators': [
-        {name: 'Gribi strādāt?', page: 'work'}, //buttons be  name => which page to
+        {name: 'Gribi strādāt?', page: 'Kādas darbības plāno veikt?'}, //buttons be  name => which page to
         {name: 'Gribi atpūsties?', page: 'rest'},
-        {name: 'Gribi izklaidēties', page: 'fun'}
+        {name: 'Gribi izklaidēties?', page: 'fun'}
     ],
-    'work': [
-        {name: 'mc', page: 'fun'},
+    'Kādas darbības plāno veikt?': [
+        {name: 'Gramatika', page: 'fun'},
+        {name: 'Kods', page: 'fun'},
+        {name: 'Plānošana', page: 'fun'},
+        {name: 'Tabulas', page: 'fun'},
+        {name: 'Dokumenti', page: 'fun'}
     ],
     'rest': [
 
@@ -26,46 +30,51 @@ switch_page('Web Akinators');
 function switch_page(page_name) {
     const buttons = document.getElementById('Buttons');
 
-    if (this.value) {
-        const page_name_from_button = this.value;
-        const page = pages[page_name_from_button];
+    if (this.value) {  //clicked
+        const page_name_from_button = this.value; //read this.val
+        const page = pages[page_name_from_button]; //check the value in pages, where does it "click"
 
-        if (!page) {
+        if (!page) { //if page doesn't exist, apdirsies
             return;
         }
-        buttons.innerHTML = '';
+        buttons.innerHTML = ''; //deletes the previous buttons
 
-        const QuestionTitle = document.getElementById('question');
-        QuestionTitle.textContent = page_name_from_button;
+        const QuestionTitle = document.getElementById('question'); //h1 as variabale
+        QuestionTitle.textContent = page_name_from_button; //the h1 is now the set, next, title
 
         for (let i = 0; i < page.length; i++ ) {
-            const button = document.createElement('button');
-            button.class = 'button';
+            const button = document.createElement('button'); // button = button ahh variable
+
+
+
+            button.class = 'button'; //links css to buttons so they look kumalala
+            // button.class links the class button.id links the css id, just have to make sure hists unique
+
             button.value = page[i].page;
-            button.textContent = page[i].name;
+            button.textContent = page[i].name; //new button uzraksti
 
-            button.onclick = switch_page;
+            button.onclick = switch_page; // click button => wtich page
 
-            buttons.appendChild(button);
+            buttons.appendChild(button); //add the new button to the div
         }
-    } else {
-        const page = pages[page_name];
+    } else { //called by code
+        const page = pages[page_name]; //acceses buttons
 
-        if (!page) {
+        if (!page) { //if page doesn't exist, apdirsies
             return;
         }
-        buttons.innerHTML = '';
+        buttons.innerHTML = ''; //deletes the previous buttons
 
         const QuestionTitle = document.getElementById('question');
         QuestionTitle.textContent = page_name;
 
         for (let i = 0; i < page.length; i++ ) {
-            const button = document.createElement('button');
+            const button = document.createElement('button');// button = button ahh variable
             button.class = 'button';
             button.value = page[i].page;
             button.textContent = page[i].name;
 
-            button.onclick = switch_page;
+            button.onclick = switch_page; //same shi
 
             buttons.appendChild(button);
         }
@@ -73,3 +82,5 @@ function switch_page(page_name) {
 
 
 }
+
+

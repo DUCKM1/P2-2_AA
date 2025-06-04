a4c07530eaa29b73ddcc828cbee2aec9d3417e8c
DUCKM1 <aandersons007@gmail.com>
2025-06-04 10:16:07 +0300

    .23

--- scripts.js
+++ scripts.js
@@ -1,7 +1,75 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button1 = document.getElementById('Work_Button');
-    const button2 = document.getElementById('Rest_Button');
-    const button3 = document.getElementById('Fun_Button');
 
 
-});
+let pages = {
+    'Web Akinators': [
+        {name: 'Gribi strādāt?', page: 'work'}, //buttons be  name => which page to
+        {name: 'Gribi atpūsties?', page: 'rest'},
+        {name: 'Gribi izklaidēties', page: 'fun'}
+    ],
+    'work': [
+        {name: 'mc', page: 'fun'},
+    ],
+    'rest': [
+
+    ],
+    'fun': [
+
+    ]
+};
+
+// <button id="Work_Button" onClick="on_work_button()">Gribās strādāt?</button>
+// <button id="Rest_Button" onClick="on_rest_Button()">Gribās atpūsties?</button>
+// <button id="Fun_Button" onClick="on_fun_Button()">Gribāš izklaidēties?</button>
+
+switch_page('Web Akinators');
+
+function switch_page(page_name) {
+    const buttons = document.getElementById('Buttons');
+
+    if (this.value) {
+        const page_name_from_button = this.value;
+        const page = pages[page_name_from_button];
+
+        if (!page) {
+            return;
+        }
+        buttons.innerHTML = '';
+
+        const QuestionTitle = document.getElementById('question');
+        QuestionTitle.textContent = page_name_from_button;
+
+        for (let i = 0; i < page.length; i++ ) {
+            const button = document.createElement('button');
+            button.class = 'button';
+            button.value = page[i].page;
+            button.textContent = page[i].name;
+
+            button.onclick = switch_page;
+
+            buttons.appendChild(button);
+        }
+    } else {
+        const page = pages[page_name];
+
+        if (!page) {
+            return;
+        }
+        buttons.innerHTML = '';
+
+        const QuestionTitle = document.getElementById('question');
+        QuestionTitle.textContent = page_name;
+
+        for (let i = 0; i < page.length; i++ ) {
+            const button = document.createElement('button');
+            button.class = 'button';
+            button.value = page[i].page;
+            button.textContent = page[i].name;
+
+            button.onclick = switch_page;
+
+            buttons.appendChild(button);
+        }
+    }
+
+
+}

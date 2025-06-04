c86bc51701956115cd3bbdf88581793d49f56b1b
DUCKM1 <aandersons007@gmail.com>
2025-06-04 08:54:02 +0300

    .23

--- scripts.js
+++ scripts.js
@@ -3,23 +3,5 @@ document.addEventListener('DOMContentLoaded', function() {
     const button2 = document.getElementById('Rest_Button');
     const button3 = document.getElementById('Fun_Button');
 
-    // Add click event listeners to each button
-    button1.addEventListener('click', function() {
-        Fall(button1, button2, button3);
-    });
 
-    button2.addEventListener('click', function() {
-        Fall(button1, button2, button3);
-    });
-
-    button3.addEventListener('click', function() {
-        Fall(button1, button2, button3);
-    });
 });
-
-function Fall(button1, button2, button3) {
-    // Add the fall class to all buttons
-    button1.classList.add('fall');
-    button2.classList.add('fall');
-    button3.classList.add('fall');
-}
